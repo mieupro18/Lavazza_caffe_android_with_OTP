b1bafc86760931d643319dc9512d37a928145a3f
Jaskar <jaskarj@mieupro.com>
2020-06-27 08:03:42 +0530

    developing stage

--- index.js
+++ index.js
@@ -2,14 +2,8 @@
  * @format
  */
 
-<<<<<<< Updated upstream
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-=======
 import { AppRegistry } from "react-native";
 import App from "./App";
 import { name as appName } from "./app.json";
->>>>>>> Stashed changes
 
 AppRegistry.registerComponent(appName, () => App);
