959fedb670fbbb807ba4bc9e4cd9a03df4f370d7
Jaskar <jaskarj@mieupro.com>
2020-08-13 11:43:00 +0530

    update

--- transformer.js
+++
@@ -1,9 +0,0 @@
-const obfuscatingTransformer = require('react-native-obfuscating-transformer');
-const filter = filename => {
-  return filename.startsWith('src');
-};
-
-module.exports = obfuscatingTransformer({
-  filter: filter,
-  trace: true,
-});
